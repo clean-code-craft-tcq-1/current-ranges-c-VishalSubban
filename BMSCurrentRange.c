b1148bf2d417978fea97cb434e9c08f7e6dcfd2d
Vishal Subban <Subban.Vishal@in.bosch.com>
2021-04-29 22:47:06 +0530

    Update 4

--- BMSCurrentRange.c
+++ BMSCurrentRange.c
@@ -6,8 +6,14 @@
 
 int BMSCurrent_InRange(int numOfSamples,int *BMSCurrentRange,int *currentRange)
 {
-    int count = 0;
-
+    int i,count = 0;
+    for(i=0;i<numOfSamples;i++)
+    {
+        if((BMSCurrentRange[i] >= currentRange[0]) && (BMSCurrentRange[i] <= currentRange[1]))
+        {
+            count++;
+        }
+    }
     return count;
 }
 
