cf53ed1ffb53cfd46ccc17d320e634c316cb9432
Vishal Subban <Subban.Vishal@in.bosch.com>
2021-04-29 22:58:02 +0530

    Update 7

--- BMSCurrentRangeTest.cpp
+++ BMSCurrentRangeTest.cpp
@@ -9,3 +9,10 @@ TEST_CASE("To check if total current samples out of range") {
     int currentRange[] ={1,5};
   REQUIRE(BMSCurrent_InRange(numofSamples,BMSCurrentRange,currentRange) == 5);
 }
+
+TEST_CASE("To check if total current samples are in range") {
+    int BMSCurrentRange[] = {3, 3, 5, 4, 10, 11, 12};
+    int numofSamples = sizeof(BMSCurrentRange) / sizeof(BMSCurrentRange[0]);
+    int currentRange[] ={1,5};
+  REQUIRE(BMSCurrent_InRange(numofSamples,BMSCurrentRange,currentRange) == 5);
+}
