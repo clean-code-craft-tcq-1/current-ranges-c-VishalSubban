55198194437969a6fef0504058a05143c56f1df9
Vishal Subban <Subban.Vishal@in.bosch.com>
2021-04-29 22:44:54 +0530

    Update 3

---
+++ BMSCurrentRange.c
@@ -0,0 +1,14 @@
+#include "BMSCurrentRange.h"
+
+#ifndef BMSCURRENTRANGE_C
+#define BMSCURRENTRANGE_C
+
+
+int BMSCurrent_InRange(int numOfSamples,int *BMSCurrentRange,int *currentRange)
+{
+    int count = 0;
+
+    return count;
+}
+
+#endif /* BMSCURRENTRANGE_C */
